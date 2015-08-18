a364ce9bfe7ae39bb0ae46a63ecb7f5a6ea3c22f
Andres Rodriguez <andresrc@gmail.com>
2015-08-18 18:37:40 +0200

    Initial project structure

---
+++ chess/src/main/java/net/derquinse/tcus/chess/ChessChallenge.java
@@ -0,0 +1,17 @@
+package net.derquinse.tcus.chess;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Chess Challenge main class.
+ * @author Andres Rodriguez
+ */
+public final class ChessChallenge {
+
+	public static void main(String[] args) {
+		// TODO
+		checkNotNull(args);
+		System.out.println("Chess Challenge main method");
+
+	}
+}
