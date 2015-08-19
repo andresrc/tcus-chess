09923177f4a073cc63d00c8a725af1cebf1dac09
Andres Rodriguez <andresrc@gmail.com>
2015-08-19 15:48:45 +0200

    StateBuilder

--- chess/src/main/java/net/derquinse/tcus/chess/solver/Position.java
+++ chess/src/main/java/net/derquinse/tcus/chess/solver/Position.java
@@ -17,7 +17,9 @@ package net.derquinse.tcus.chess.solver;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
+import java.util.BitSet;
 import java.util.Objects;
 
 /**
@@ -45,12 +47,12 @@ public final class Position {
 		checkArgument(index >= 0 && index < size.getPositions(), "Illegal index %d for %s", index, size);
 		this.index = index;
 	}
-	
+
 	/** Returns the position as an index in the board. */
 	public int getIndex() {
 		return index;
 	}
-	
+
 	/** Returns the row coordinate. */
 	public int getRow() {
 		return index / size.getColumns();
@@ -60,24 +62,88 @@ public final class Position {
 	public int getColumn() {
 		return index % size.getColumns();
 	}
-	
+
+	/** Creates a state builder based on the current position. */
+	StateBuilder builder() {
+		return new StateBuilder();
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(size, index);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Position) {
-			final Position other = (Position)obj;
+			final Position other = (Position) obj;
 			return index == other.index && size.equals(other.size);
 		}
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.format("Position[%d](%d,%d)-of-%s", index, getRow(), getColumn(), size);
 	}
-	
+
+	/**
+	 * Class used by pieces to build their state starting from the current position.
+	 * All threaten operations are independent and start from the piece position.
+	 */
+	final class StateBuilder {
+		private final BitSet bitSet = new BitSet();
+		/** Whether the state has been built. */
+		private boolean built = false;
+
+		private StateBuilder() {
+			bitSet.set(index);
+		}
+
+		private void checkNotBuilt() {
+			checkState(!built, "State already built");
+		}
+
+		private Integer delta(int steps, int nRows, int nColumns) {
+			checkNotBuilt();
+			checkArgument(steps > 0, "The number of steps must be > 0");
+			int row = getRow() + steps * nRows;
+			int column = getColumn() + steps * nColumns;
+			if (row >= 0 && column >= 0 && row < size.getRows() && column < size.getColumns()) {
+				return row * size.getColumns() + column;
+			}
+			return null;
+		}
+
+		/** Virtually threaten and if the resulting position is inside the board mark as unavailable. */
+		StateBuilder threatenIfPossible(int nRows, int nColumns) {
+			Integer index = delta(1, nRows, nColumns);
+			if (index != null) {
+				bitSet.set(index);
+			}
+			return this;
+		}
+
+		/** Virtually threaten and while the resulting position is inside the board mark as unavailable. */
+		StateBuilder threatenWhilePossible(int nRows, int nColumns) {
+			for (int steps = 1;; steps++) {
+				Integer index = delta(steps, nRows, nColumns);
+				if (index != null) {
+					bitSet.set(index);
+				} else {
+					break;
+				}
+			}
+			return this;
+		}
+
+		/** Builds the state. */
+		State build() {
+			checkNotBuilt();
+			built = true;
+			return State.of(size, bitSet);
+		}
+
+	}
+
 }
---
+++ chess/src/test/java/net/derquinse/tcus/chess/solver/StateBuilderTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.derquinse.tcus.chess.solver;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests for StateBuilder.
+ * @author Andres Rodriguez
+ */
+public final class StateBuilderTest {
+
+	private static void check(State s, Integer... indexes) {
+		assertEquals(newHashSet(s), newHashSet(indexes));
+	}
+
+	/** Exercise with simple movements. */
+	@Test
+	public void simple() {
+		final Size size = Size.of(3, 3);
+		final Position p = size.getPosition(1, 1);
+		check(p.builder().build(), 0, 1, 2, 3, 5, 6, 7, 8);
+		check(p.builder().threatenIfPossible(-1, 1).build(), 0, 1, 3, 5, 6, 7, 8);
+		check(p.builder().threatenIfPossible(-1, 1).threatenIfPossible(-1, 5).build(), 0, 1, 3, 5, 6, 7, 8);
+	}
+
+	/** Exercise with continuous movements. */
+	@Test
+	public void continuous() {
+		final Size size = Size.of(4, 4);
+		final Position p = size.getPosition(1, 1);
+		check(p.builder().threatenWhilePossible(1, 0).build(), 0, 1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 14, 15);
+		check(p.builder().threatenWhilePossible(1, 0).threatenWhilePossible(-1, 0).build(), 0, 2, 3, 4, 6, 7, 8, 10, 11, 12, 14, 15);
+		check(p.builder().threatenWhilePossible(1, 0).threatenWhilePossible(-1, 0).threatenWhilePossible(1, 1).build(), 0, 2, 3, 4, 6, 7, 8, 11, 12, 14);
+	}
+	
+}
